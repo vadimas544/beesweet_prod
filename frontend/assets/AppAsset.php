6acd3370336b779cec6f42a80c6089bb6ef78d06
Vadim Samoilenko <vadim123544@gmail.com>
2019-02-12 15:26:57 +0200

    Header layout done

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -15,6 +15,7 @@ class AppAsset extends AssetBundle
         'css/style.css',
     ];
     public $js = [
+        'js/common.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -28,31 +28,63 @@ AppAsset::register($this);
 <?php $this->beginBody() ?>
 
 <!--<div class="wrap">-->
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-1"></div>
-            <div class="col-lg-2">
-                <a href="<?php echo Url::to(['site/index']); ?>">
-                   <?= Html::img('@web/img/logo2.png', ['alt' => 'Наш логотип']) ?> 
-                </a>       
-            </div>
-            <div class="col-lg-1"></div>
-            <div class="col-lg-3 text-left">
+    
+        <header>
+            <div class="container-fluid">
                 <div class="row">
-                    <a href="">праздничные тортики</a>|
-                    <a href="">капкейки</a>|
-                    <a href="">кейкпопсы</a>|
-                    <a href="">меренги</a>
+                    <div class="col-lg-1"></div>
+                    <div class="col-lg-2">
+                        <a href="<?php echo Url::to(['site/index']); ?>">
+                           <?= Html::img('@web/img/logo2.png', ['alt' => 'Наш логотип']) ?> 
+                        </a>       
+                    </div>
+                    <div class="col-lg-4 text-left">
+                        <div class="row">
+                            &nbsp
+                        </div>
+                        <div class="header-list">
+                            <div class="row">
+                                <a href="<?php echo Url::to(['site/index']); ?>">праздничные тортики</a> |
+                                <a href="<?php echo Url::to(['site/index']); ?>">капкейки</a> |
+                                <a href="<?php echo Url::to(['site/index']); ?>">кейкпопсы</a> |
+                                <a href="<?php echo Url::to(['site/index']); ?>">меренги</a> 
+                            </div>
+                            <br />
+                            <div class="row slogun">Закажи сладость своей мечты!</div>
+                            <br />
+                            <div class="row">Очень красивая цитата из жизни про тортики</div>
+                            </div>    
+                    </div>
+                    <div class="col-lg-1"></div>   
+                    <div class="col-lg-3">
+                        <div class="header-socials text-center">
+                            <div class="row">&nbsp</div><br /><br />
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <p>(063)-948-93-54</p>
+                                </div>
+                                <div class="col-lg-6">
+                                    <a href="">обратная связь</a>
+                                </div>
+                            </div>
+                            <br />
+                            <div class="row">
+                                <div class="col-lg-4">&nbsp</div>
+                                <div class="col-lg-4">
+                                    <a href="">FB</a>
+                                </div>
+                                <div class="col-lg-4">
+                                    <a href="">IG</a>
+                                </div>
+                            </div>
+                        </div>  
+                        <br />
+                    </div>
+                    <div class="col-lg-1"></div>
                 </div>
-                <div class="row">Закажи сладость своей мечты!</div>
-                <div class="row">Очень красивая цитата из жизни про тортики</div>
             </div>
-            <div class="col-lg-3">
-                
-            </div>
-            <div class="col-lg-1"></div>
-        </div>
-    </div>
+        </header>
+        
 
     <?php
 
