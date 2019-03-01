93d845d6a73ea2c286f254a96cd6e251433221a4
Vadim Samoilenko <vadim123544@gmail.com>
2019-03-01 07:42:57 +0200

    adm-auth v1.0

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -7,13 +7,14 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
-$this->title = 'Login';
+// echo Yii::$app->security->generatePasswordHash('trrytr03');
+$this->title = 'Вход';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out the following fields to login:</p>
+    <!-- <p>Please fill out the following fields to login:</p> -->
 
     <div class="row">
         <div class="col-lg-5">
--- common/models/LoginForm.php
+++ common/models/LoginForm.php
@@ -31,6 +31,15 @@ class LoginForm extends Model
         ];
     }
 
+    public function attributeLabels()
+    {
+        return [
+            'username' => 'Логин',
+            'password' => 'Пароль',
+            'rememberMe' => 'Запомнить'
+        ];
+    }
+
     /**
      * Validates the password.
      * This method serves as the inline validation for password.
